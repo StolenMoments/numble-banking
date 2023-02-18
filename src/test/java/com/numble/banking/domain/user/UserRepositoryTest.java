1c8b075ebdf7646558674e1a6ef8d6d7e83b49b1
lee <unknown9732@naver.com>
2023-02-19 02:09:12 +0900

    test : UserRepository 테스트 생성

---
+++ src/test/java/com/numble/banking/domain/user/UserRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.numble.banking.domain.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    void testSaveUser() {
+        // given
+        User user = User.builder().userName("TestUser").password("1234").loginId("LOGIN_ID")
+            .build();
+
+        // when
+        repository.save(user);
+
+        // then
+        User savedUser = repository.findByLoginId("LOGIN_ID");
+        assertThat(savedUser).isEqualTo(user);
+    }
+
+}
