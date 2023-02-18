9124b2d27fdb0163a6c29adc803f8bfea72df9a8
lee <unknown9732@naver.com>
2023-02-19 03:20:58 +0900

    feat : UserCreateRequestDto toEntity 메서드 추가

--- src/main/java/com/numble/banking/dto/UserCreateRequestDto.java
+++ src/main/java/com/numble/banking/dto/UserCreateRequestDto.java
@@ -1,11 +1,14 @@
 package com.numble.banking.dto;
 
+import com.numble.banking.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @ToString
 public class UserCreateRequestDto {
@@ -19,5 +22,13 @@ public class UserCreateRequestDto {
         this.loginId = loginId;
         this.password = password;
         this.userName = userName;
+    }
+
+    public User toEntity() {
+        return User.builder()
+            .loginId(this.loginId)
+            .password(this.getPassword())
+            .userName(this.getUserName())
+            .build();
     }
 }
