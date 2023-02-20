682f2ad853fecd532739a115af519d09715d6063
lee <unknown9732@naver.com>
2023-02-20 21:23:52 +0900

    feat : UserRepository findByLoginId 리턴 타입 변경

--- src/main/java/com/numble/banking/domain/user/UserRepository.java
+++ src/main/java/com/numble/banking/domain/user/UserRepository.java
@@ -1,10 +1,11 @@
 package com.numble.banking.domain.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
 }
