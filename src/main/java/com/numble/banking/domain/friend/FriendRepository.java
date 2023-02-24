408359c2b321fb123882e197b09d05e8f8fccf01
lee <unknown9732@naver.com>
2023-02-24 20:41:31 +0900

    feat : FriendRepository 제네릭 타입 변경

--- src/main/java/com/numble/banking/domain/friend/FriendRepository.java
+++ src/main/java/com/numble/banking/domain/friend/FriendRepository.java
@@ -1,10 +1,9 @@
 package com.numble.banking.domain.friend;
 
-import com.numble.banking.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FriendRepository extends JpaRepository<Friend, User> {
+public interface FriendRepository extends JpaRepository<Friend, FriendRelationKey> {
 
 }
