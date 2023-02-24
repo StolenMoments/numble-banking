3e357df7f7687e5699539bf5fc400daff574933a
lee <unknown9732@naver.com>
2023-02-24 20:00:08 +0900

    feat : AccountRepository 메서드 추가

--- src/main/java/com/numble/banking/domain/account/AccountRepository.java
+++ src/main/java/com/numble/banking/domain/account/AccountRepository.java
@@ -1,9 +1,19 @@
 package com.numble.banking.domain.account;
 
+import com.numble.banking.domain.user.User;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByUser(User user);
+    Optional<Account> findByUser_LoginId(String loginId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Account> findByAccountId(Long accountId);
 
 }
