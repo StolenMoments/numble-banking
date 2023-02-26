2cad2b0fd2695ef8f27f0d12e0d5649b1246fe6c
lee <unknown9732@naver.com>
2023-02-26 18:58:31 +0900

    feat : UserRepository - 동시성 이슈 대책으로 @Lock 추가

--- src/main/java/com/numble/banking/domain/user/UserRepository.java
+++ src/main/java/com/numble/banking/domain/user/UserRepository.java
@@ -1,11 +1,13 @@
 package com.numble.banking.domain.user;
 
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<User> findByLoginId(String loginId);
 }
