263ac648dc9f2617d82614c58d2518a2aa26d8af
lee <unknown9732@naver.com>
2023-02-27 16:32:42 +0900

    test : 약식 동시성 테스트 작성

---
+++ src/test/java/com/numble/banking/service/AccountConcurrencyTest.java
@@ -0,0 +1,128 @@
+package com.numble.banking.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.numble.banking.domain.account.AccountRepository;
+import com.numble.banking.dto.AccountCreateRequestDto;
+import com.numble.banking.dto.AccountDepositRequestDto;
+import com.numble.banking.dto.AccountTransferRequestDto;
+import com.numble.banking.dto.FriendAddRequestDto;
+import com.numble.banking.dto.UserCreateRequestDto;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountConcurrencyTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private FriendService friendService;
+
+    long senderUserId;
+    long receiverUserId;
+    long senderAccountId;
+    long receiverAccountId;
+
+    @BeforeEach
+    void setup() {
+        UserCreateRequestDto userCreateRequestDto1 = UserCreateRequestDto.builder()
+            .loginId("sender").password("1234").userName("john").build();
+        senderUserId = userService.createUser(userCreateRequestDto1);
+
+        UserCreateRequestDto userCreateRequestDto2 = UserCreateRequestDto.builder()
+            .loginId("receiver").password("1234").userName("steven").build();
+        receiverUserId = userService.createUser(userCreateRequestDto2);
+
+        AccountCreateRequestDto accountCreateRequestDto1 = AccountCreateRequestDto.builder()
+            .loginId("sender").amount(10000L).build();
+        senderAccountId = accountService.createAccount(accountCreateRequestDto1);
+
+        AccountCreateRequestDto accountCreateRequestDto2 = AccountCreateRequestDto.builder()
+            .loginId("receiver").amount(0L).build();
+        receiverAccountId = accountService.createAccount(accountCreateRequestDto2);
+
+        friendService.addFriend(FriendAddRequestDto.builder()
+            .fromLoginId("sender")
+            .toLoginId("receiver")
+            .build());
+    }
+
+    @AfterEach
+    void cleanUp() {
+        accountRepository.deleteAll();
+    }
+
+    @Test
+    void depositTestWithConcurrency() throws InterruptedException {
+
+        long amount = 500L;
+        int numberOfThreads = 10;
+
+        ExecutorService exeService = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        for (int i = 0; i < numberOfThreads; i++) {
+            exeService.execute(() -> {
+                accountService.depositMoney(
+                    AccountDepositRequestDto.builder()
+                        .accountId(senderAccountId)
+                        .amount(amount)
+                        .build());
+
+                latch.countDown();
+            });
+        }
+        latch.await();
+
+        Long balance = accountService.getBalance(senderAccountId);
+        assertThat(balance).isEqualTo(
+            10000L + (amount * numberOfThreads)
+        );
+    }
+
+    @Test
+    void transferTestWithConcurrency() throws InterruptedException {
+
+        long amount = 500L;
+        int numberOfThreads = 10;
+
+        ExecutorService exeService = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        for (int i = 0; i < numberOfThreads; i++) {
+            exeService.execute(() -> {
+                try {
+                    accountService.transferMoney(
+                        AccountTransferRequestDto.builder()
+                            .senderAccountId(senderAccountId)
+                            .receiverAccountId(receiverAccountId)
+                            .receiverLoginId("receiver")
+                            .loginId("sender")
+                            .amount(amount)
+                            .build());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Long senderBalance = accountService.getBalance(senderAccountId);
+        Long receiverBalance = accountService.getBalance(receiverAccountId);
+        assertThat(receiverBalance).isEqualTo(amount * numberOfThreads);
+        assertThat(senderBalance).isEqualTo(10000L - (amount * numberOfThreads));
+    }
+}
